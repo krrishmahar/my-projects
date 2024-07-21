0c00fd5e0821c3c137c4094f79dc83cb8a36e625
krrishmahar <krrishmahar5@gmail.com>
2024-07-21 23:23:55 +0530

    search feature added

--- Week4_Python_Prep/microblog/app/main/forms.py
+++ Week4_Python_Prep/microblog/app/main/forms.py
@@ -1,3 +1,4 @@
+from flask import request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import ValidationError, DataRequired, Length
@@ -6,6 +7,16 @@ from flask_babel import _, lazy_gettext as _l
 from app import db
 from app.models import User
 
+
+class SearchForm(FlaskForm):
+    q = StringField(_l('Search'), validators=[DataRequired()])
+
+    def __init__(self, *args, **kwargs):
+        if 'formdata' not in kwargs:
+            kwargs['formdata'] = request.args
+        if 'meta' not in kwargs:
+            kwargs['meta'] = {'csrf': False}
+        super(SearchForm, self).__init__(*args, **kwargs)
 
 class EditProfileForm(FlaskForm):
     username = StringField(_l('Username'), validators=[DataRequired()])
